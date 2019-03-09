18b69d74d8124a5644cb370b2fab279584276894
Claudio Cortese <claudio.cortese@outlook.it>
2019-03-09 13:11:38 +0100

    test: add functional tests

--- index.js
+++ index.js
@@ -8,31 +8,28 @@ const { PORT, MONGODB_URI } = process.env
 
 mongoose.set('useFindAndModify', false)
 
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
-    express()
-      .use(
-        helmet({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'"],
-            },
-          },
-        })
-      )
-      .use(cors())
-      .use(express.json())
-      .use(
-        express.urlencoded({
-          extended: false,
-        })
-      )
-      .use('/api/stock-prices', stockPricesRouter)
-      .listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-      })
-  })
-  .catch(err => {
-    console.error(err)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).catch(err => {
+  console.error(err)
+})
+
+module.exports = express()
+  .use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+        },
+      },
+    })
+  )
+  .use(cors())
+  .use(express.json())
+  .use(
+    express.urlencoded({
+      extended: false,
+    })
+  )
+  .use('/api/stock-prices', stockPricesRouter)
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
   })
